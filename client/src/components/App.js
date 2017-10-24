ac7e86389d2008284ace5df7a73e104fa3e5842c
asmm.dan <asmm.dan@gmail.com>
2017-10-24 16:23:48 -0700

    first-build

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+class App extends Component {
+     render(){ 
+      return (
+        <div>
+          <h1>Dan React Boilerplate</h1>
+        </div>
+    )
+  }
+}
+
+export default App;
