38d163776baeb2d0917f06bee4e87283b37c5b3c
asmm.dan <asmm.dan@gmail.com>
2017-10-27 19:48:51 -0700

    first build

--- client/src/components/firebase/firebase.js
+++ client/src/components/firebase/firebase.js
@@ -1,15 +1,14 @@
 import * as firebase from 'firebase';
-import runtimeEnv from '@mars/heroku-js-runtime-env';
 
 const env = runtimeEnv();
 
 const config = {
-    apiKey: env.FIREBASE_API_KEY,
-    authDomain: env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: env.FIREBASE_DATABASE_URL,
-    projectId: env.FIREBASE_PROJECT_ID,
-    storageBucket: env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
