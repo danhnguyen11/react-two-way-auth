7d10a7d41ffce57df2450e5cd059298a71b5a7a5
asmm.dan <asmm.dan@gmail.com>
2017-10-27 19:14:17 -0700

    first build

--- client/src/components/firebase/firebase.js
+++ client/src/components/firebase/firebase.js
@@ -9,7 +9,7 @@ const config = {
     databaseURL: env.FIREBASE_DATABASE_URL,
     projectId: env.FIREBASE_PROJECT_ID,
     storageBucket: env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID
+    messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
